de7dbb82849e685733ee0eac5a442fe29ca4d873
Robert Schwarz <mail@rschwarz.net>
2018-02-20 17:29:01 +0100

    add header according to package-lint

--- sql-clickhouse.el
+++ sql-clickhouse.el
@@ -1,15 +1,41 @@
 ;;; sql-clickhouse.el --- support ClickHouse as SQL interpreter
 
+;; Copyright (c) 2018 Robert Schwarz
+;; Package-Requires: ((emacs "24"))
+;; Package-Version: 0.1
 ;; Author: Robert Schwarz <mail@rschwarz.net>
-;; URL: https://github.com/leethargo/sql-clickhouse
-
+;; Homepage: https://github.com/leethargo/sql-clickhouse
+;;
+;; Licensed under MIT License.
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;; Commentary:
-;; Add new product type ClickHouse for SQL mode in Emacs.
+
+;; Add new product type ClickHouse for SQL mode in Emacs. Defines extra
+;; keywords, types and functions for font-lock as well as the client for
+;; comint-mode.
+
+;;; Code:
 
 (require 'sql)
 
-;;; Code:
 
 (defvar sql-clickhouse-font-lock-keywords
   (list
