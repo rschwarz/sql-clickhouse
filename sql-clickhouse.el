5dd180975b69fc2351b65364c1f2eb59ec587ac0
Robert Schwarz <mail@rschwarz.net>
2018-02-20 10:35:05 +0100

    rename xyz -> clickhouse

--- sql-clickhouse.el
+++ sql-clickhouse.el
@@ -7,63 +7,63 @@
 
 ;; 1) Add the product to the list of known products.
 
-(sql-add-product 'xyz "XyzDB"
+(sql-add-product 'clickhouse "ClickhouseDB"
                  '(:free-software t))
 
 ;; 2) Define font lock settings.  All ANSI keywords will be
 ;;    highlighted automatically, so only product specific keywords
 ;;    need to be defined here.
 
-(defvar my-sql-mode-xyz-font-lock-keywords
+(defvar my-sql-mode-clickhouse-font-lock-keywords
   '(("\\b\\(red\\|orange\\|yellow\\)\\b"
      . font-lock-keyword-face))
-  "XyzDB SQL keywords used by font-lock.")
+  "ClickhouseDB SQL keywords used by font-lock.")
 
-(sql-set-product-feature 'xyz
+(sql-set-product-feature 'clickhouse
                          :font-lock
-                         'my-sql-mode-xyz-font-lock-keywords)
+                         'my-sql-mode-clickhouse-font-lock-keywords)
 
 ;; 3) Define any special syntax characters including comments and
 ;;    identifier characters.
 
-(sql-set-product-feature 'xyz
+(sql-set-product-feature 'clickhouse
                          :syntax-alist ((?# . "_")))
 
 ;; 4) Define the interactive command interpreter for the database
 ;;    product.
 
-(defcustom my-sql-xyz-program "ixyz"
-  "Command to start ixyz by XyzDB."
+(defcustom my-sql-clickhouse-program "iclickhouse"
+  "Command to start iclickhouse by ClickhouseDB."
   :type 'file
   :group 'SQL)
 
-(sql-set-product-feature 'xyz
-                         :sqli-program 'my-sql-xyz-program)
-(sql-set-product-feature 'xyz
-                         :prompt-regexp "^xyzdb> ")
-(sql-set-product-feature 'xyz
+(sql-set-product-feature 'clickhouse
+                         :sqli-program 'my-sql-clickhouse-program)
+(sql-set-product-feature 'clickhouse
+                         :prompt-regexp "^clickhousedb> ")
+(sql-set-product-feature 'clickhouse
                          :prompt-length 7)
 
 ;; 5) Define login parameters and command line formatting.
 
-(defcustom my-sql-xyz-login-params '(user password server database)
-  "Login parameters to needed to connect to XyzDB."
+(defcustom my-sql-clickhouse-login-params '(user password server database)
+  "Login parameters to needed to connect to ClickhouseDB."
   :type 'sql-login-params
   :group 'SQL)
 
-(sql-set-product-feature 'xyz
-                         :sqli-login 'my-sql-xyz-login-params)
+(sql-set-product-feature 'clickhouse
+                         :sqli-login 'my-sql-clickhouse-login-params)
 
-(defcustom my-sql-xyz-options '("-X" "-Y" "-Z")
-  "List of additional options for `sql-xyz-program'."
+(defcustom my-sql-clickhouse-options '("-X" "-Y" "-Z")
+  "List of additional options for `sql-clickhouse-program'."
   :type '(repeat string)
   :group 'SQL)
 
-(sql-set-product-feature 'xyz
-                         :sqli-options 'my-sql-xyz-options))
+(sql-set-product-feature 'clickhouse
+                         :sqli-options 'my-sql-clickhouse-options))
 
-(defun my-sql-comint-xyz (product options)
-  "Connect ti XyzDB in a comint buffer."
+(defun my-sql-comint-clickhouse (product options)
+  "Connect ti ClickhouseDB in a comint buffer."
 
   ;; Do something with `sql-user', `sql-password',
   ;; `sql-database', and `sql-server'.
@@ -80,12 +80,12 @@
           options)))
     (sql-comint product params)))
 
-(sql-set-product-feature 'xyz
-                         :sqli-comint-func 'my-sql-comint-xyz)
+(sql-set-product-feature 'clickhouse
+                         :sqli-comint-func 'my-sql-comint-clickhouse)
 
 ;; 6) Define a convenience function to invoke the SQL interpreter.
 
-(defun my-sql-xyz (&optional buffer)
-  "Run ixyz by XyzDB as an inferior process."
+(defun my-sql-clickhouse (&optional buffer)
+  "Run iclickhouse by ClickhouseDB as an inferior process."
   (interactive "P")
-  (sql-product-interactive 'xyz buffer))
+  (sql-product-interactive 'clickhouse buffer))
